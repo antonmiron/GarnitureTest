df19af486c26cdf4f3891aada992241ee71bb40e
Anton Mironov <mironovaa@elcomplus.ru>
2022-04-05 14:21:45 +0500

    simple audiorecorder

--- app/src/main/java/com/example/garnituretest/MainActivity.kt
+++ app/src/main/java/com/example/garnituretest/MainActivity.kt
@@ -31,9 +31,6 @@ class MainActivity : AppCompatActivity() {
     private val minBufferSize = AudioRecord.getMinBufferSize(8000, AudioFormat.CHANNEL_IN_MONO, AudioFormat.ENCODING_PCM_16BIT)
     private var audioRecord: AudioRecord? = null
     private val audioTrack = createAudioTrack()
-    private lateinit var broadcast: Broadcast
-    private val audioManager: AudioManager by lazy{ getSystemService(Context.AUDIO_SERVICE) as AudioManager }
-    private val adapter = Adapter()
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,28 +38,6 @@ class MainActivity : AppCompatActivity() {
         viewBinding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(viewBinding.root)
 
-        broadcast = Broadcast(audioManager){
-            val status = when(it){
-                -1 -> "ERROR"
-                0 -> "DISCONNECTED"
-                1 -> {
-                    job = lifecycleScope.launch {
-                        viewBinding.tvStatus.text = "REC"
-                        recAudio()
-                        viewBinding.tvStatus.text = "STOP"
-                    }
-
-                    "CONNECT"
-                }
-                2 -> "CONNECTING"
-                else -> "UNDEFINED - $it"
-            }
-
-            adapter.addItem("BLT_SCO: $status")
-
-
-        }
-        registerReceiver(broadcast, IntentFilter().apply { addAction(AudioManager.ACTION_SCO_AUDIO_STATE_UPDATED) })
 
         micPermissionGranted = ContextCompat.checkSelfPermission(
             this,
@@ -71,19 +46,11 @@ class MainActivity : AppCompatActivity() {
 
 
         prepareUI()
-
-        val devices = audioManager.getDevices(AudioManager.GET_DEVICES_INPUTS)
-        devices.forEach {
-            Log.d("VVV", "it.type: ${it.type}, it.name: ${it.productName}")
-        }
-
     }
 
 
     private fun prepareUI(){
         with(viewBinding){
-            rvBluetoothState.adapter = adapter
-
             /**REC**/
             btnRec.setOnClickListener {
                 job?.cancel()
@@ -93,20 +60,16 @@ class MainActivity : AppCompatActivity() {
                     return@setOnClickListener
                 }
 
-                audioManager.isBluetoothScoOn = true
-                audioManager.startBluetoothSco()
-
-                adapter.addItem("audioManager.isBluetoothScoOn: ${audioManager.isBluetoothScoOn}")
-                Log.d("VVV","audioManager.isBluetoothScoOn: ${audioManager.isBluetoothScoOn}")
+                job = lifecycleScope.launch {
+                    tvStatus.text = "REC"
+                    recAudio()
+                }
             }
 
             /**STOP**/
             btnStop.setOnClickListener {
                 audioRecord?.stop()
                 audioTrack.stop()
-
-                audioManager.isBluetoothScoOn = false
-                audioManager.stopBluetoothSco()
 
                 job?.cancel()
                 job = lifecycleScope.launch {
